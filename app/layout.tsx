ae0aa550008707e2145baef09a1ec8891c706e5c
Tayyab Abdullah <tabdullah0801@gmail.com>
2026-04-15 16:10:36 +0500

    Used Auth Provider/Context in the layout file.

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata: Metadata = {
 };
 
 import StoreProvider from "@/lib/redux/StoreProvider";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function RootLayout({
   children,
@@ -30,7 +31,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </StoreProvider>
       </body>
     </html>
   );
